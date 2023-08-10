289f5a114053f4c10f5f182eb1b6f6b0e4f13d81
Inflection-prashat <prashant.kharade@inflectionzone.com>
2023-08-10 13:56:48 +0530

    last commit

--- src/service/search.service.js
+++ src/service/search.service.js
@@ -1,16 +1,16 @@
 
 export const getResponse = async (req) => {
-    return " get response is from service layer"
+    return " get response is from service layer";
 }
 
 export const postResponse = async (req) => {
-    return " post response is from service layer"
+    return " post response is from service layer";
 }
 export const putResponse = async (req) => {
-    return " put response is from service layer"
+    return " put response is from service layer";
 }
 export const deleteResponse = async (req) => {
-    return " delete response is from service layer"
+    return " delete response is from service layer";
 }
 
 
